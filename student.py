62241731832b3e393c6e2a3226d3129236c3ff4c
JackO117 <125601341+JackO117@users.noreply.github.com>
2023-05-25 19:04:39 +0000

    .

--- student.py
+++ student.py
@@ -258,7 +258,7 @@ class Piggy(PiggyParent):
 
     def maze(self):
       while self.read_distance() > 120: 
-            self.fwd(40, 34)
+            self.fwd(40, 37)
             time.sleep(0.5)
       self.stop()
       self.decision()
